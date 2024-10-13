d28ab634b5c5fd6e4059ab45066620a6cb1142fb
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-10-13 12:28:00 +0300

    Suite tests

---
+++ demos/d01.gotest/factorial_fixture_test.go
@@ -0,0 +1,38 @@
+package demos
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// The Suite type
+// Embeds also the asserts package
+type FactorialSuite struct {
+    suite.Suite
+    theFactorial Factorial
+}
+
+// Runs before each test
+func (suite *FactorialSuite) BeforeTest() {
+    suite.theFactorial = Factorial{}
+}
+
+func (suite *FactorialSuite) TestFactorialTests() {
+    suite.Equal(1, suite.theFactorial.Calculate(1))
+    suite.Equal(2, suite.theFactorial.Calculate(2))
+    suite.Equal(6, suite.theFactorial.Calculate(3))
+}
+
+func (suite *FactorialSuite) TestNegativeFactorial() {
+	suite.T().Skip("Not implemented yet")
+    suite.Equal(0, suite.theFactorial.Calculate(-3))
+}
+
+func (suite *FactorialSuite) TestBigFactorial() {
+    suite.Equal(3628800, suite.theFactorial.Calculate(10))
+}
+
+func TestFactorialSuite(t *testing.T) {
+    suite.Run(t, new(FactorialSuite))
+}
