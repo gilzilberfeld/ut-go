176f84f363ad05c3c69f4ddacae9b07526586ea2
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-10-13 11:53:21 +0300

    with testify

--- demos/d01.gotest/factorial_test.go
+++ demos/d01.gotest/factorial_test.go
@@ -2,38 +2,36 @@ package demos
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFactorial(t *testing.T) {
 	factorial := Factorial{}
 	result := factorial.Calculate(1)
+	// testing package, manual check
 	if result != 1 {
 		t.Errorf("Expected 1, got %d", result)
 	}
+
 	result = factorial.Calculate(2)
-	if result != 2 {
-		t.Errorf("Expected 2, got %d", result)
-	}
+	// testify, with additional error message
+	assert.Equal(t, 2, result, "DAMMIT! Expected 2, got %d", result)
 	result = factorial.Calculate(3)
-	if result != 6 {
-		t.Errorf("Expected 6, got %d", result)
-	}
+	// testify, without error message
+	assert.Equal(t, 6, result)
 }
 
 func TestNegativeFactorial(t *testing.T) {
 	t.Skip("Not implemented yet")
 	factorial := Factorial{}
 	result := factorial.Calculate(-3)
-	if result != 0 {
-		t.Errorf("Expected 0, but got %d", result)
-	}
+	assert.Equal(t, 0, result)
 }
 
 func TestBigFactorial(t *testing.T) {
 	factorial := Factorial{}
 	result := factorial.Calculate(10)
 	expected := 3628800
-	if result != expected {
-		t.Errorf("Expected %d, but got %d", expected, result)
-	}
+	assert.Equal(t, expected, result)
 }
