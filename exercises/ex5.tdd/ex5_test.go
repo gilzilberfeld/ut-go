3247ea00738cbe0cf39597a5cbe47d840130a7c4
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-10-14 15:30:20 +0300

    Ex 5-6

---
+++ exercises/ex5.tdd/ex5_test.go
@@ -0,0 +1,15 @@
+package exercises
+
+import (
+	"testing"
+)
+
+// For input 3, create the following diamond:
+//      A
+//     B B
+//    C   C
+//     B B
+//      A
+
+func TestFive(t *testing.T) {
+}
---
+++ exercises/ex6.final/ex6_test.go
@@ -0,0 +1,12 @@
+package exercises
+
+import (
+	"testing"
+)
+
+// Build the CalculatorDisplay from scratch in TDD
+// Organize code and tests
+// And don't forget to refactor
+
+func TestFinal(t *testing.T) {
+}
